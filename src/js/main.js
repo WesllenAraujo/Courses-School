41bf047f61d64c2247f63919df422697c2fe8d2e
Wesllen Araújo <wesllenaraujocarmo2007@gmail.com>
2023-09-14 21:44:13 -0300

    Added the sub courses menu event

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,16 @@ let courses = document.getElementById("courseButton");
 let coursesMenu = document.querySelector(".courses-menu-navigation");
 let coursesArrow = document.getElementById("main-arrow");
 
+let subCoursesMenu = document.getElementById("subCourses");
+let tecnoCoursesMenu = document.getElementById("tecMenu");
+let interCoursesMenu = document.getElementById("interMenu");
+
+let tecCourse = document.getElementById("technology");
+let interCourse = document.getElementById("interpesonal");
+
+let tecArrow = document.getElementById("tecArrow");
+let interArrow = document.getElementById("interArrow");
+
 courses.addEventListener("click", function() {
     if(coursesMenu.style.display === "none") {
         coursesMenu.style.display = "flex";
@@ -9,5 +19,29 @@ courses.addEventListener("click", function() {
     } else {
         coursesMenu.style.display = "none";
         coursesArrow.style.transform = "rotate(0deg)";
+    }
+})
+
+tecCourse.addEventListener("click", function() {
+    if(tecnoCoursesMenu.style.display === "none") {
+        subCoursesMenu.style.display = "flex";
+        tecnoCoursesMenu.style.display = "flex";
+        tecArrow.style.transform = "rotate(180deg)";
+    } else {
+        subCoursesMenu.style.display = "none";
+        tecnoCoursesMenu.style.display = "none";
+        tecArrow.style.transform = "rotate(0deg)";
+    }
+})
+
+interCourse.addEventListener("click", function() {
+    if(interCoursesMenu.style.display === "none") {
+        subCoursesMenu.style.display = "flex";
+        interCoursesMenu.style.display = "flex";
+        interArrow.style.transform = "rotate(180deg)";
+    } else {
+        subCoursesMenu.style.display = "none";
+        interCoursesMenu.style.display = "none";
+        interArrow.style.transform = "rotate(0deg)";
     }
 })
