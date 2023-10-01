c69bfdcd59a90f8a1ff420317731e9d97eea30a6
Wesllen Araújo <wesllenaraujocarmo2007@gmail.com>
2023-10-01 19:56:40 -0300

    Updated the courses's menu code

--- src/js/main.js
+++ src/js/main.js
@@ -1,70 +1,67 @@
-let courses = document.getElementById("courseButton");
-let coursesMenu = document.querySelector(".courses-menu-navigation");
-let coursesArrow = document.getElementById("main-arrow");
+var { coursesNavigation, subCoursesNavigation, subCoursesMenu, subCoursesArrow, courses, coursesMenu, mainCourseArrow, emailButton } = variables();
 
-let subCoursesMenu = document.getElementById("subCourses");
-let tecnoCoursesMenu = document.getElementById("tecMenu");
-let interCoursesMenu = document.getElementById("interMenu");
+showSubCourses();
 
-let tecCourse = document.getElementById("technology");
-let interCourse = document.getElementById("interpesonal");
+showCourses();
 
-let tecArrow = document.getElementById("tecArrow");
-let interArrow = document.getElementById("interArrow");
+emailValidate();
 
-courses.addEventListener("click", function() {
-    if(coursesMenu.style.display === "none") {
-        coursesMenu.style.display = "flex";
-        coursesArrow.style.transform = "rotate(180deg)";
-    } else {
-        coursesMenu.style.display = "none";
-        coursesArrow.style.transform = "rotate(0deg)";
-    }
-})
+function emailValidate() {
+    emailButton.addEventListener("click", function () {
+        const email = document.getElementById('email').value;
+        const regex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        const result = regex.test(email);
 
-tecCourse.addEventListener("click", function() {
-    if(tecnoCoursesMenu.style.display === "none") {
-        subCoursesMenu.style.display = "flex";
-        tecnoCoursesMenu.style.display = "flex";
-        tecArrow.style.transform = "rotate(180deg)";
-    } else {
-        subCoursesMenu.style.display = "none";
-        tecnoCoursesMenu.style.display = "none";
-        tecArrow.style.transform = "rotate(0deg)";
-    }
-})
+        let successMessage = document.getElementById("emailSuccess");
+        let errorMessage = document.getElementById("emailError");
 
-interCourse.addEventListener("click", function() {
-    if(interCoursesMenu.style.display === "none") {
-        subCoursesMenu.style.display = "flex";
-        interCoursesMenu.style.display = "flex";
-        interArrow.style.transform = "rotate(180deg)";
-    } else {
-        subCoursesMenu.style.display = "none";
-        interCoursesMenu.style.display = "none";
-        interArrow.style.transform = "rotate(0deg)";
-    }
-})
-
-function validate() {
-    
+        if (result === false) {
+            successMessage.style.display = "none";
+            errorMessage.style.display = "flex";
+        } else {
+            errorMessage.style.display = "none";
+            successMessage.style.display = "flex";;
+        }
+    });
 }
 
-let emailButton = document.getElementById("contact-button");
+function showCourses() {
+    courses.addEventListener("click", function () {
+        if (coursesMenu.style.display === "none") {
+            coursesMenu.style.display = "flex";
+            mainCourseArrow.classList.add("arrow-active");
+        } else {
+            coursesMenu.style.display = "none";
+            subCoursesMenu.style.display = "none";
+            mainCourseArrow.classList.remove("arrow-active");
+        }
+    });
+}
 
-emailButton.addEventListener("click", function() {
-    const email = document.getElementById('email').value;
-    const regex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    const result = regex.test(email);
-
-    let successMessage = document.getElementById("emailSuccess");
-    let errorMessage = document.getElementById("emailError");
-
-    if(result === false) {
-        successMessage.style.display = "none";
-        errorMessage.style.display = "flex";
-    } else {
-        errorMessage.style.display = "none";
-        successMessage.style.display = "flex";;
+function showSubCourses() {
+    for (let i = 0; i < coursesNavigation.length; i++) {
+        coursesNavigation[i].addEventListener("click", function () {
+            if (window.getComputedStyle(subCoursesNavigation[i]).display === "none") {
+                subCoursesMenu.style.display = "block";
+                subCoursesNavigation[i].style.display = "flex";
+                subCoursesArrow[i].classList.add("arrow-active");
+            } else {
+                subCoursesMenu.style.display = "none";
+                subCoursesNavigation[i].style.display = "none";
+                subCoursesArrow[i].classList.remove("arrow-active");
+            }
+        });
     }
-})
+}
+
+function variables() {
+    var coursesNavigation = document.querySelectorAll('.course-navigation');
+    var subCoursesNavigation = document.querySelectorAll(".subMenu");
+    var subCoursesMenu = document.getElementById("subCourses");
+    let courses = document.getElementById("courseButton");
+    let coursesMenu = document.querySelector(".courses-menu-navigation");
+    let subCoursesArrow = document.querySelectorAll(".subArrow");
+    let mainCourseArrow = document.getElementById("main-arrow");
+    let emailButton = document.getElementById("contact-button");
+    return { coursesNavigation, subCoursesNavigation, subCoursesMenu, subCoursesArrow, courses, coursesMenu, mainCourseArrow, emailButton };
+}
